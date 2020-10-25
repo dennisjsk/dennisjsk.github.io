a218268921d735c5f93ca694ad220b31df8b4a26
Dennis J Sukumaran <dennis.sukumaran@logicline.de>
2020-10-26 00:53:52 +0530

    Cloned from my heroku site

---
+++ main.js
@@ -0,0 +1,10 @@
+$('document').ready(function(){
+    console.log('ready');
+    // var $window = $(window),
+    // $stickyEl = $('#the-sticky-div'),
+    // elTop = $stickyEl.offset().top;
+
+    // $window.scroll(function() {
+    //     $stickyEl.toggleClass('sticky', $window.scrollTop() > elTop);
+    // });
+});
