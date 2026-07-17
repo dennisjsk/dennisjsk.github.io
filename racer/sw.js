dc275fcbd9367522abb4865da52aa8524fafc12f
Dennis J S <dennisjsk@gmail.com>
2026-07-18 02:50:32 +0530

    feat: rename app

--- racer/sw.js
+++ racer/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = '8bit-racer-v1';
+const CACHE_NAME = 'autobahn-racer-v1';
 const ASSETS = ['/', './', 'index.html', 'manifest.json', 'favicon.svg', 'icon-192.png', 'icon-512.png'];
 
 self.addEventListener('install', (e) => {
